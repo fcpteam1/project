90ced623ead3664a6cc16095c6d0f00cbb93d923
Shane <Shane@Eaaaaaaaaaagle>
2014-08-13 11:55:29 +0100

    Customer,Product,Stock,User Test done

---
+++ src/SupplierTest.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SupplierTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSupplier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSupplierStringIntArrayListOfProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProducts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProducts() {
+		fail("Not yet implemented");
+	}
+
+}
