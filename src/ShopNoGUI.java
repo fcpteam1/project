d8ff2c3ce9e5d623028c404c11b34ed9d2f5ee6b
Shane <Shane@Eaaaaaaaaaagle>
2014-08-13 12:45:42 +0100

    ShopNoGUI login menu started

--- src/ShopNoGUI.java
+++ src/ShopNoGUI.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+
 
 public class ShopNoGUI {
-
+	private ArrayList<User> user = new ArrayList<User>();
+	
+	public ShopNoGUI(){
+		
+	}
+	
+	public void createUsers(){
+		User user1 = new User("ShaneFlynn88","Password11",123,true);
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new ShopNoGUI();
 
 	}
 
