8a812c6979b06ba787de323f25bd4acd47f12193
bombay1982 <andrzej.gda@gmail.com>
2014-08-25 13:54:20 +0100

    Fixed increasing of the textfields in LoginGUI. Size of textfields are now constant.

--- src/GUI/LoginGUI.java
+++ src/GUI/LoginGUI.java
@@ -1,6 +1,9 @@
 package GUI;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -25,18 +28,36 @@ public class LoginGUI  {
 		
 		submit=new JButton("SUBMIT");
 		loginPanel=new JPanel();
-		loginPanel.setLayout(new GridLayout(3,2,5,10));
+		loginPanel.setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+		
 		loginUsername=new JTextField(20);
 		loginPassword=new JPasswordField(20);
-		usernameLabel=new JLabel("username:");
-		passwordLabel=new JLabel("password:");
+		usernameLabel=new JLabel("Username:  ");
+		passwordLabel=new JLabel("Password:  ");
 		
+		gc.gridx = 0;
+		gc.gridy = 0;
+		gc.weightx = 1;
+		gc.weighty = 1;
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.LINE_END;
+		loginPanel.add(usernameLabel, gc);
 		
-		loginPanel.add(usernameLabel);
-		loginPanel.add(loginUsername);
-		loginPanel.add(passwordLabel);
-		loginPanel.add(loginPassword);
+		gc.gridx = 1;
+		gc.gridy = 0;
+		gc.anchor = GridBagConstraints.LINE_START;
+		loginPanel.add(loginUsername, gc);
 		
+		gc.gridx = 0;
+		gc.gridy = 1;
+		gc.anchor = GridBagConstraints.LINE_END;
+		loginPanel.add(passwordLabel, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
+		loginPanel.add(loginPassword, gc);
 		
 		loginSubmitPanel=new JPanel();
 		loginSubmitPanel.setLayout(new FlowLayout());
